2859a9aad85d2ba36d503e808e8c1bde61a03ed0
Mohanlaltrichy <90626602+Mohanlaltrichy@users.noreply.github.com>
2023-12-12 20:20:32 +0530

    Subscription Login Page Redirect

--- Modules/global_templates/Views/dashboard.php
+++ Modules/global_templates/Views/dashboard.php
@@ -29,8 +29,28 @@ echo view('\Modules\global_templates\Views\global_header',$data); // Header File
                 <div class="card" style="background-color:#662483;">
                     <div onclick="open_cloudconnector()" target="_blank"> 
                     <img src="<?php echo base_url(); ?>assets/images/cloud_logo.png" style='padding: 10px 10px;' width='100px;' height='100px;' alt="user-img">
-                        <div class="card-heading1 text-right">                                               
+                        <div class="card-heading1 text-right" style="font-size: 15px;">                                               
                             <b>Cloud Connector</b>                         
+                        </div>                     
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-xl-2">
+                <div class="card" style="background-color:#0E7833;">
+                    <div onclick="open_subscription()" target="_blank"> 
+                    <img src="<?php echo base_url(); ?>assets/images/subscription.png" style='padding: 10px 10px;' width='100px;' height='100px;' alt="user-img">
+                        <div class="card-heading1 text-right" style="font-size: 15px;">                                               
+                            <b>Subscription</b>                         
+                        </div>                     
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-xl-2">
+                <div class="card" style="background-color:#EA5B0B;">
+                    <div onclick="open_alert_and_notify()" target="_blank"> 
+                    <img src="<?php echo base_url(); ?>assets/images/alert_and_notify.png" style='padding: 10px 10px;' width='100px;' height='100px;' alt="user-img">
+                        <div class="card-heading1 text-right" style="font-size: 15px;">                                               
+                            <b>Alert And Notification</b>                     
                         </div>                     
                     </div>
                 </div>
@@ -54,8 +74,19 @@ echo view('\Modules\global_templates\Views\global_footer'); // Footer File Inclu
 <!-- Form Validation Code Plugin End -->
 
 <script>
+        //Open Cloud Connector Module
         function open_cloudconnector() {
             window.open('http://localhost:9000/login/user_login_key_validation/<?=$login_key;?>', '_blank');
+        }
+
+        //Open Subscription Module
+        function open_subscription() {
+            window.open('http://localhost:8000/user_login_key_validation/<?=$login_key;?>', '_blank');
+        }
+
+        //Open Alert And Notification
+        function open_alert_and_notify() {
+            window.open('http://localhost:8000/user_login_key_validation/<?=$login_key;?>', '_blank');
         }
     </script>
 
